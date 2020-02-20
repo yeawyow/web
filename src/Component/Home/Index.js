8b89be5ce0ed2f8a8ce44510a31e1162dccff303
yeawyow <yeawyow@gmail.com>
2020-02-20 14:38:21 +0700

    new icon link

--- src/Component/Home/Index.js
+++ src/Component/Home/Index.js
@@ -1,40 +1,60 @@
 
 import React from 'react';
 import { Container,Row,Col,Image } from 'react-bootstrap';
+
 import Putsadu from '../Images/Icon/putsadu.png'; // Tell Webpack this JS file uses this image
 import Inpatient from '../Images/Icon/inpatient.png'; 
 import Airoffice from '../Images/Icon/airoffice.png'; 
 import Iclaim from '../Images/Icon/iclaim.png'; 
 import Carsystem from '../Images/Icon/carsystem.png'; 
 
+
+
 export const Home= ()=>(
   <React.Fragment>
   
  
-  <Container>
+   <Container>
   <Row>
-    <Col>
     
-    <Image src={Carsystem} rounded />
-    <div className="desc">จองรถออนไลน์</div>
+    <Col>
+      <a  href="http://akathospital.com/carsystem"  rel=" noopener noopener">
+    <Image src={Carsystem} rounded /></a>
+    <div className="card2">จองรถออนไลน์</div>
+    </Col>
+    
+    
+    
+    <Col>
+    <a href="http://akathospital.com/iclaim"   rel=" noopener noopener">
+    <Image src={Iclaim} rounded /></a>
+    <div className="desc">Iclaim</div>
+    
     </Col>
     <Col>
-    <Image src={Iclaim} rounded />
-    <div className="desc">Iclaim</div></Col>
+    <a href="http://akathospital.com/airoffice"  rel="noopener noopener">
+    <Image src={Airoffice} rounded /></a>
+    <div className="desc">Air-Office</div>
+    
+    </Col>
     <Col>
-    <Image src={Airoffice} rounded />
-    <div className="desc">Air-Office</div></Col>
-    <Col>
-    <Image src={Inpatient} rounded />
+    <a href="http://akathospital.com/inpatient"   rel="noopener noopener">
+    <Image src={Inpatient} rounded /></a>
     <div className="desc">ระบบรายงานจำนวนผู้ป่วยใน
-</div></Col>
+</div>
+
+</Col>
     <Col>
-    <Image src={Putsadu} rounded />
-    <div className="desc">คลังพัสดุ</div></Col>
+    <a href="http://google.com"  rel="noopener noopener">
+    <Image src={Putsadu} rounded /></a>
+    <div className="desc">คลังพัสดุ</div>
+    
+    </Col>
+    
   </Row>
 
   </Container>
-
+ 
 
   </React.Fragment>
    
--- src/Component/Jumbotron.js
+++ src/Component/Jumbotron.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import {Jumbotron,Container} from 'react-bootstrap';
+import {Jumbotron as Jumbo,Container} from 'react-bootstrap';
+import styled from 'styled-components';
+import hosband from '../Component/Images/hosband.jpg';
 
-
-export const Jumbo = ()=>(
-<Jumbotron>
+const Styles = styled.div`
+ .jumbo{
+  background:url(${hosband}) no-repeat fixed bottom;
+  background-size:cover;
+  height: 350px;
+  position:relative;
+  z-index:-2;
+ }
+ color:white;
+`;
+export const Jumbotron = ()=>(
+  <Styles>
+    <Jumbo fluid className="jumbo">
 
       <Container>
          <h1>โรงพยาบาลอากาศอำนวย ยินดีต้อนรับ</h1>
@@ -16,6 +28,8 @@ export const Jumbo = ()=>(
   </p>
       </Container>
  
-</Jumbotron>
+</Jumbo>
+  </Styles>
+
 
 )
