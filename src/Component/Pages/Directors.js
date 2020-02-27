4497511f115c00d71d7f3d7645d5b2e260327c1f
yeawyow <yeawyow@gmail.com>
2020-02-27 21:54:50 +0700

    director

--- src/Component/Pages/Directors.js
+++ src/Component/Pages/Directors.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {  Container} from 'react-bootstrap';
+import {  Container,Row,Col} from 'react-bootstrap';
 
 
 
 export const Directors =()=>(
      <Container>
          <h1>คณะกรรมการบริหารโรงพยาบาล</h1>
+        
+         <Row>
+    <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
+  </Row>
+
      </Container>
     
 )
